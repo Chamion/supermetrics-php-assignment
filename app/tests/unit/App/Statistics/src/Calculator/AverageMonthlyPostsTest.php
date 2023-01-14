afa200d80dd353c911339d02124829c0bcb4b08e
Jemi Salo <jemi.salo@gmail.com>
2023-01-14 12:06:29 +0200

    Assert sets units

--- app/tests/unit/App/Statistics/src/Calculator/AverageMonthlyPostsTest.php
+++ app/tests/unit/App/Statistics/src/Calculator/AverageMonthlyPostsTest.php
@@ -113,4 +113,20 @@ class AverageMonthlyPostsTest extends TestCase
             $result->getName()
         );
     }
+    public function testSetsUnits(): void
+    {
+        $start = DateTime::createFromFormat('F, Y', 'December, 2022')->modify('first day of this month')->setTime(0, 0, 0);
+        $end = DateTime::createFromFormat('F, Y', 'January, 2023')->modify('last day of this month')->setTime(23, 59, 59);
+        $params = (new ParamsTo())
+            ->setStatName(StatsEnum::AVERAGE_POSTS_NUMBER_PER_USER_PER_MONTH)
+            ->setStartDate($start)
+            ->setEndDate($end);
+        $calculator = new AverageMonthlyPosts();
+        $calculator->setParameters($params);
+        $result = $calculator->calculate();
+        $this->assertEquals(
+            'posts',
+            $result->getChildren()[0]->getUnits()
+        );
+    }
 }

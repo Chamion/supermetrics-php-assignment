bec3b76ddf7b2a270aa187f9cd37c670dcc41393
Jemi Salo <jemi.salo@gmail.com>
2023-01-14 10:48:36 +0200

    Assert calculates child for each month

--- app/tests/unit/App/Statistics/src/Calculator/AverageMonthlyPostsTest.php
+++ app/tests/unit/App/Statistics/src/Calculator/AverageMonthlyPostsTest.php
@@ -19,9 +19,9 @@ use Statistics\Enum\StatsEnum;
  */
 class AverageMonthlyPostsTest extends TestCase
 {
-    public function testCalculatesAnything(): void
+    public function testCalculatesChildForEachMonth(): void
     {
-        $start = DateTime::createFromFormat('F, Y', 'January, 2023')->modify('first day of this month')->setTime(0, 0, 0);
+        $start = DateTime::createFromFormat('F, Y', 'December, 2022')->modify('first day of this month')->setTime(0, 0, 0);
         $end = DateTime::createFromFormat('F, Y', 'January, 2023')->modify('last day of this month')->setTime(23, 59, 59);
         $params = (new ParamsTo())
             ->setStatName(StatsEnum::AVERAGE_POSTS_NUMBER_PER_USER_PER_MONTH)
@@ -30,6 +30,9 @@ class AverageMonthlyPostsTest extends TestCase
         $calculator = new AverageMonthlyPosts();
         $calculator->setParameters($params);
         $result = $calculator->calculate();
-        $this->assertIsObject($result);
+        $this->assertEquals(
+            2,
+            count($result->getChildren())
+        );
     }
 }
